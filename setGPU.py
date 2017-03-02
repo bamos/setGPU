764a19745681a43e95b77fb50442829db183808e
Brandon Amos <bamos@cs.cmu.edu>
2017-03-02 14:39:57 -0500

    Also update the best GPU for #1.

--- setGPU.py
+++ setGPU.py
@@ -8,6 +8,7 @@ for gpu in stats:
     ratio = float(gpu.entry['memory.used'])/float(gpu.entry['memory.total'])
     if ratio < bestRatio:
         bestRatio = ratio
+        bestGPU = int(gpu.entry['index'])
 
 print("setGPU: Setting GPU to: {}".format(bestGPU))
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
