6b873db88a326397a00feeb07ae1e56fe3132c3a
Brandon Amos <bdamos@vt.edu>
2017-03-10 15:35:51 -0500

    Inline ratios computation.

--- setGPU.py
+++ setGPU.py
@@ -3,9 +3,7 @@ import gpustat
 
 stats = gpustat.GPUStatCollection.new_query()
 ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(
-    lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total'])
-    stats)
+ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
 bestGPU = min(zip(ids, ratios), key=lambda x: x[1])[0]
 
 print("setGPU: Setting GPU to: {}".format(bestGPU))
