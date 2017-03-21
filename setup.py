1bde0d9b51ee4adfc48d49a7bac3dc3523fc4fdd
Brandon Amos <bamos@cs.cmu.edu>
2017-03-21 12:49:49 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='setGPU',
-    version='0.0.6',
+    version='0.0.7',
     description="A small Python library that automatically sets CUDA_VISIBLE_DEVICES to the least-loaded GPU on multi-GPU systems.",
     author='Brandon Amos',
     author_email='bamos@cs.cmu.edu',
